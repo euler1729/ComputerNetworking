44e97e3ade9ec288351cc4f4e0c455ef0c8f4ff6
euler1729 <mahmudul.hhh@gmail.com>
2023-02-05 01:31:48 +0600

    Server Request timeout

--- Lab04/src/main/java/client/Client.java
+++ Lab04/src/main/java/client/Client.java
@@ -4,33 +4,39 @@ import java.util.Scanner;
 
 public class Client {
     private static final int port = 1234;
+
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         //1. Socket to carry data
-        DatagramSocket datagramSocket = new DatagramSocket();
+        DatagramSocket serverSocket = new DatagramSocket();
         InetAddress server_ip = InetAddress.getLocalHost();
-        if(args.length>0){
+        if (args.length > 0) {
             server_ip = InetAddress.getByName(args[0]);
         }
-        while(true){
-            System.out.print("Enter the Hostname: ");
-            String in = scanner.nextLine();
-            byte [] buffer = in.getBytes();
-            //2. Create DatagramPacket to send data
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, server_ip, port);
-            //3. calling send method of DatagramSocket to send data
-            datagramSocket.send(packet);
+        while (true) {
+            try {
+                System.out.print("Enter the Hostname: ");
+                String in = scanner.nextLine();
+                byte[] buffer = in.getBytes();
+                //2. Create DatagramPacket to send data
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length, server_ip, port);
+                //3. calling send method of DatagramSocket to send data
+                serverSocket.send(packet);
+                serverSocket.setSoTimeout(5000);
+                if (in.equals("quit")) {
+                    serverSocket.close();
+                    break;
+                }
 
-            if(in.equals("quit")){
-                datagramSocket.close();
-                break;
+                byte[] receivedByte = new byte[65535];
+                DatagramPacket receivedPacket = new DatagramPacket(receivedByte, receivedByte.length);
+                serverSocket.receive(receivedPacket);
+                String receivedString = new String(receivedPacket.getData(), 0, receivedPacket.getLength());
+                System.out.println("IP Address: " + receivedString);
+            } catch (Exception e) {
+                System.out.println("Server request time out. Please send request once again...");
             }
 
-            byte[]receivedByte = new byte[65535];
-            DatagramPacket receivedPacket = new DatagramPacket(receivedByte, receivedByte.length);
-            datagramSocket.receive(receivedPacket);
-            String receivedString = new String(receivedPacket.getData(),0,receivedPacket.getLength());
-            System.out.println("IP Address: "+receivedString);
         }
     }
 }
