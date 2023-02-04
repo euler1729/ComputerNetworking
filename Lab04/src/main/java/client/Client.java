3c36866df6cd70e46eac7601dd24e5bfb670f0ec
euler1729 <mahmudul.hhh@gmail.com>
2023-02-05 01:13:26 +0600

    Any dns resolution added

--- Lab04/src/main/java/client/Client.java
+++ Lab04/src/main/java/client/Client.java
@@ -8,25 +8,29 @@ public class Client {
         Scanner scanner = new Scanner(System.in);
         //1. Socket to carry data
         DatagramSocket datagramSocket = new DatagramSocket();
-        InetAddress ip = InetAddress.getLocalHost();
+        InetAddress server_ip = InetAddress.getLocalHost();
+        if(args.length>0){
+            server_ip = InetAddress.getByName(args[0]);
+        }
         while(true){
+            System.out.print("Enter the Hostname: ");
             String in = scanner.nextLine();
-            byte [] buffer = in.getBytes();//Converts String to byte array
+            byte [] buffer = in.getBytes();
             //2. Create DatagramPacket to send data
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, ip, port);
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, server_ip, port);
             //3. calling send method of DatagramSocket to send data
             datagramSocket.send(packet);
-
-            byte[]receivedByte = new byte[65535];
-            DatagramPacket receivedPacket = new DatagramPacket(receivedByte, receivedByte.length);
-            datagramSocket.receive(receivedPacket);
-            String receivedString = new String(receivedPacket.getData(),0,receivedPacket.getLength());
-            System.out.println("Server: "+receivedString);
 
             if(in.equals("quit")){
                 datagramSocket.close();
                 break;
             }
+
+            byte[]receivedByte = new byte[65535];
+            DatagramPacket receivedPacket = new DatagramPacket(receivedByte, receivedByte.length);
+            datagramSocket.receive(receivedPacket);
+            String receivedString = new String(receivedPacket.getData(),0,receivedPacket.getLength());
+            System.out.println("IP Address: "+receivedString);
         }
     }
 }
--- Lab04/src/main/java/server/Server.java
+++ Lab04/src/main/java/server/Server.java
@@ -1,37 +1,48 @@
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.HashMap;
+import java.util.Objects;
 
 public class Server {
-
+    private static final HashMap<String,String> ip_hash = new HashMap<String, String>();
     private static final int port = 1234;
+    private static final String[] hosts = {"zoho.com", "gmail.com","google.com", "facebook.com"};
+    private static final String[] ip = {"172.28.251.59", "172.217.11.5","172.217.11.14","31.13.71.36"};
+    static {
+        for(int i=0; i<hosts.length; ++i){
+            ip_hash.put(hosts[i],ip[i]);
+        }
+    }
     public static void main(String[] args) throws IOException {
         //1. Socket to listen on port - 1234
-        DatagramSocket datagramSocket = new DatagramSocket(port);
+        DatagramSocket serverSocket = new DatagramSocket(port);
         while(true){
             byte[] receive = new byte[65535];
             //2. DatagramPacket to receive data
             DatagramPacket received_packet = new DatagramPacket(receive, receive.length);
             //3. Receiving data in byte buffer
-            datagramSocket.receive(received_packet);
-            System.out.println("Client: "+
-                    new String(received_packet.getData(),0, received_packet.getLength()));
-            if(ToString(receive).toString().equals("quit")){
+            serverSocket.receive(received_packet);
+            //Data sent by Client
+            String received_data = new String(received_packet.getData(),0, received_packet.getLength());
+            //Client Ip address
+            InetAddress ip_client = received_packet.getAddress();
+            //Clients' Port
+            int port_client = received_packet.getPort();
+
+            if(received_data.equals("quit")){
                 System.out.println("Client leaving...");
-                datagramSocket.close();
+                serverSocket.close();
                 break;
             }
-            datagramSocket.send(received_packet);
+            InetAddress inetAddress = java.net.InetAddress.getByName(received_data);
+            //String ip = ip_hash.get(received_data);
+            String ip = inetAddress.toString();
+            String send_msg = Objects.requireNonNullElse(ip, "Host not found");
+            DatagramPacket send_pack = new DatagramPacket(send_msg.getBytes(),
+                                        send_msg.getBytes().length,ip_client,port_client);
+            serverSocket.send(send_pack);
         }
-    }
-    //Converts byte array to string
-    public static StringBuilder ToString(byte[] b){
-        if(b==null) return null;
-        StringBuilder str = new StringBuilder();
-        int i = 0;
-        while(b[i]!=0){
-            str.append((char)b[i++]);
-        }
-        return str;
     }
 }
